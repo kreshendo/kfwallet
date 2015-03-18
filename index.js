9d76f9e2025bcca167007fa8b3d64b2bcaeed893
kreshendo <thekreshendo@gmail.com>
2015-03-17 22:05:37 -0500

    commit message

---
+++ index.js
@@ -0,0 +1,11 @@
+var Kfwallet  = new function() {
+    
+    
+    this.data = {
+        youtubeUrl: null,
+        privKey1: null,
+        pubKey1: null,
+        winningSeed: null,
+         
+    };
+}
